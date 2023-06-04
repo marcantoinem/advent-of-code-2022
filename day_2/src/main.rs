39b94205f3459e714105d906f7915469503362fc
lemark <marcantoine_manningham@outlook.com>
2023-06-04 17:56:03 -0400

    Day 2 completed

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,12 +1,13 @@
 use std::fs;
 
-#[derive(PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq, Debug)]
 enum Choice {
     Rock,
     Paper,
     Scissor,
 }
 
+#[derive(Debug)]
 enum MatchResult {
     Win,
     Lose,
@@ -28,6 +29,17 @@ impl Choice {
             | (Choice::Paper, Choice::Rock)
             | (Choice::Scissor, Choice::Paper) => MatchResult::Win,
             (_, _) => MatchResult::Lose,
+        }
+    }
+    fn obtain_result(&self, result: &MatchResult) -> Self {
+        match (result, self) {
+            (MatchResult::Equal, choice) => *choice,
+            (MatchResult::Lose, Choice::Paper) => Choice::Rock,
+            (MatchResult::Win, Choice::Scissor) => Choice::Rock,
+            (MatchResult::Lose, Choice::Scissor) => Choice::Paper,
+            (MatchResult::Win, Choice::Rock) => Choice::Paper,
+            (MatchResult::Lose, Choice::Rock) => Choice::Scissor,
+            (MatchResult::Win, Choice::Paper) => Choice::Scissor,
         }
     }
 }
@@ -55,18 +67,32 @@ fn score(text: &str) -> usize {
         "Z" => Choice::Scissor,
         _ => unreachable!(),
     };
-    println!("{:?} {:?}", first_choice, our_choice);
+    our_choice.to_value() + our_choice.compare_with_player(&first_choice).to_value()
+}
+
+fn score_part2(text: &str) -> usize {
+    let first_choice = match &text[0..1] {
+        "A" => Choice::Rock,
+        "B" => Choice::Paper,
+        "C" => Choice::Scissor,
+        _ => unreachable!(),
+    };
+    let match_result = match &text[2..3] {
+        "X" => MatchResult::Lose,
+        "Y" => MatchResult::Equal,
+        "Z" => MatchResult::Win,
+        _ => unreachable!(),
+    };
+    let our_choice = first_choice.obtain_result(&match_result);
+    println!("{:?} {:?} {:?}", first_choice, match_result, our_choice);
     our_choice.to_value() + our_choice.compare_with_player(&first_choice).to_value()
 }
 
 fn main() {
     let result = fs::read_to_string("input.txt")
-        // let result = "A Y
-        // B X
-        // // C Z"
         .unwrap()
         .lines()
-        .map(score)
+        .map(score_part2)
         .sum::<usize>();
     println!("{}", result);
 }
